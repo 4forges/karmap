33bdede1699fdcd18c4cfbc30296a33c9b9e899b
Michele Ferri <sako@Micheles-iMac-2.local>
2017-03-23 15:16:33 +0100

    moved spec here

---
+++ spec/karmap/models/test_service.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+class TestService < Karma::Service
+
+  include Karma::ServiceMessage
+
+  min_running  2
+  max_running  5
+  port         33000
+  auto_restart false
+
+  num_threads  2
+
+  def perform
+    msg = "#{Time.now} :: process #{$$} :: thread #{Thread.current.object_id} :: method #{__method__}"
+    File.open("#{folder}/#{name}-#{__method__}.log", 'w') { |file| file.puts(msg) }
+    Thread.current[:logger].info(msg) rescue ""
+  end
+
+  def before_start
+    File.open("#{folder}/#{name}-#{__method__}.log", 'w') { |file| file.puts("process #{$$} :: thread #{Thread.current.object_id} :: method #{__method__}") }
+  end
+
+  def after_start
+    File.open("#{folder}/#{name}-#{__method__}.log", 'w') { |file| file.puts("process #{$$} :: thread #{Thread.current.object_id} :: method #{__method__}") }
+  end
+
+  def before_stop
+    File.open("#{folder}/#{name}-#{__method__}.log", 'w') { |file| file.puts("process #{$$} :: thread #{Thread.current.object_id} :: method #{__method__}") }
+  end
+
+  def after_stop
+    File.open("#{folder}/#{name}-#{__method__}.log", 'w') { |file| file.puts("process #{$$} :: thread #{Thread.current.object_id} :: method #{__method__}") }
+  end
+
+  private ##########
+
+  def folder
+    'log/'
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'karmap'
+require 'karmap/models/concerns/service_message'
 require 'karmap/models/test_service'
+require 'karmap/models/mock_service'
 require 'rspec'
-
-def example_folder
-  '~/Karma/spec'
-end
+require 'rspec/wait'
 
 def resource_path(filename)
   File.expand_path("../resources/#{filename}", __FILE__)
@@ -18,7 +17,33 @@ end
 RSpec.configure do |config|
 
   config.color = true
-
   config.order = 'rand'
+
+  config.before(:each) do
+    Karma.logger = Logger.new(STDOUT)
+    Karma::Watchdog.logger = Logger.new(STDOUT)
+    Karma.configuration do |config|
+      config.user = 'extendi'
+      config.project_name = 'karmat'
+      config.karma_project_id = 'DPPQCWCPh6P4nYEQvK7EB39L'
+      config.karma_user_id = 'bc74ed7c0ec9aacf3513f3692d09a578'
+      config.aws_access_key_id = 'AKIAIU4ZJVPJ6JGQEDQQ'
+      config.aws_secret_access_key = '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'
+      # config.engine = 'system_raw'
+      config.services = [TestService]
+      # config.notifier = 'queue'
+    end
+  end
+
+  config.before(:each) do
+    allow_any_instance_of(Karma::Queue::Client).to receive(:_client) do
+      Aws::SQS::Client.new(
+        access_key_id: ENV['KARMA_AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['KARMA_AWS_SECRET_ACCESS_KEY'],
+        region: 'eu-west-1',
+        stub_responses: true
+      )
+    end
+  end
 
 end
