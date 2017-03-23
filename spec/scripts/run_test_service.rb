bbaf3d310b42dd79a47eeaf17de39d18edfd3455
Michele Ferri <sako@Micheles-iMac-2.local>
2017-03-23 16:31:32 +0100

    spec

---
+++ spec/scripts/run_test_service.rb
@@ -0,0 +1,13 @@
+require './spec/karmap/models/test_service'
+
+Karma.configuration do |config|
+  config.user = 'extendi'
+  config.project_name = 'karmat'
+  config.karma_project_id = 'DPPQCWCPh6P4nYEQvK7EB39L'
+  config.karma_user_id = 'bc74ed7c0ec9aacf3513f3692d09a578'
+  config.aws_access_key_id = 'AKIAIU4ZJVPJ6JGQEDQQ'
+  config.aws_secret_access_key = '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'
+  config.services = [TestService]
+end
+
+TestService.new.run
