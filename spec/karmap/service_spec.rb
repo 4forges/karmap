5bfc36240a5faafdc933405834bf7ba02481bc65
Michele Ferri <sako@Micheles-iMac-2.local>
2017-03-24 10:20:01 +0100

    spec

--- spec/karmap/service_spec.rb
+++ spec/karmap/service_spec.rb
@@ -23,6 +23,7 @@ describe Karma::Service do
     engine.start_service(service)
     wait_for{File.exists?('spec/log/testservice-before_start.log')}.to be_truthy
     wait_for{File.exists?('spec/log/testservice-after_start.log')}.to be_truthy
+    wait_for{File.exists?('spec/log/testservice-perform.log')}.to be_truthy
 
     status = engine.show_service(service)
     pid = status.values[0].pid
