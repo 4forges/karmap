1e4428668c97ad3100f5e478299db2b6a977c66c
Giovannelli Duccio <giovannelli@GiovannlisiMac2.homenet.telecomitalia.it>
2017-06-05 11:39:27 +0200

    Disabling watchdog enable on deploy

--- lib/karmap/engine/systemd.rb
+++ lib/karmap/engine/systemd.rb
@@ -121,10 +121,10 @@ module Karma::Engine
 
       reload
 
-      if service == Karma::Watchdog
-        instance_name = "#{service.full_name}@#{Karma.watchdog_port}.service"
-        `systemctl enable --user #{instance_name}`
-      end
+      # if service == Karma::Watchdog
+      #   instance_name = "#{service.full_name}@#{Karma.watchdog_port}.service"
+      #   `systemctl enable --user #{instance_name}`
+      # end
 
       Karma.logger.info { "#{__method__}: end systemd export for service #{service.name}" }
     end
