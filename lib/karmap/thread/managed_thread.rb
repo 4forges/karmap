4d04184de4ae895cc3f0368fe43700c34598af7e
Alessandro Brazzini <alessandro.brazzini@gmail.com>
2017-06-13 14:49:54 +0200

    set_log_level

--- lib/karmap/thread/managed_thread.rb
+++ lib/karmap/thread/managed_thread.rb
@@ -45,7 +45,7 @@ module Karma::Thread
     end
 
     def set_log_level(level)
-      Karma.logger.level = level
+      @thread[:logger].level = level if @thread[:logger].present?
     end
 
     def thread_index
